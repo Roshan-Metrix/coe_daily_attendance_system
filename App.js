c8882f86f8f07f5ca55d7da68df7b65442d20d1b
Roshan Patel <roshanpatel12309@gmail.com>
2026-07-15 23:01:47 +0530

    #Fixes dependency issue

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import "./global.css";
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import * as ImagePicker from "expo-image-picker";
+import * as Linking from "expo-linking";
+
 import AppNavigator from "./src/navigation/AppNavigator";
-import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  const requestPermissions = async () => {
+    // Camera Permission
+    const cameraPermission =
+      await ImagePicker.requestCameraPermissionsAsync();
+
+    // Media Library Permission
+    const mediaPermission =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!cameraPermission.granted) {
+      if (!cameraPermission.canAskAgain) {
+        alert("Please enable Camera permission from Settings.");
+        Linking.openSettings();
+      }
+    }
+
+    if (!mediaPermission.granted) {
+      if (!mediaPermission.canAskAgain) {
+        alert("Please enable Media Library permission from Settings.");
+        Linking.openSettings();
+      }
+    }
+  };
+
   return (
     <>
-      <StatusBar style="dark" /> 
+      <StatusBar style="dark" />
       <AppNavigator />
     </>
   );
 }
+
+// import "./global.css";
+// import AppNavigator from "./src/navigation/AppNavigator";
+// import { StatusBar } from 'expo-status-bar';
+
+// export default function App() {
+//   return (
+//     <>
+//       <StatusBar style="dark" /> 
+//       <AppNavigator />
+//     </>
+//   );
+// }
